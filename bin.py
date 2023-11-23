a80763a95ea156315260ea61d1722a79f3d4bc5a
kaubu <29447633+kaubu@users.noreply.github.com>
2023-11-23 05:17:54 +0000

    Nearly complete!

---
+++ bin.py
@@ -0,0 +1,66 @@
+"""
+Word: dere
+Ang Spelling: 
+Meaning: ᛫ hurt ᛫ harm ᛫ injury ᛬ to hurt ᛫ to harm ᛫ to injure ᛫
+Kind: N᛬V
+Forebear: dēre
+Taken from: ME
+Notes: 
+
+
+Definitions: {'N': ['hurt', 'harm', 'injury'], 'V': ['to hurt', 'to harm', 'to injure']}
+"""
+
+# split_definitions_examples = [
+#     ["᛫ delicate and tendril-like ᛫ diaphanous ᛫ gauzy ᛫ airy ᛫", "AJ"],
+#     [" 	᛫ a region in Poland ᛫", "N(P)"],
+#     [" 	᛫ guidance ᛫ instruction ᛫", "N"],
+#     ["᛫ weak ᛫ watery ᛫ feeble wavering ᛫", "AJ"],
+#     ["᛫ a manner ᛫ a fashion ᛫ a method ᛬ to make apparent ᛬ sagacious ᛫ prudent ᛫", "N᛬V᛬AJ"]
+# ]
+
+# for s in split_definitions_examples:
+#     a = split_definitions(s[0], s[1])
+#     print(a)
+
+# Form:
+# "english definition lemma": {
+#   "anglish_word": X,
+#   "anglish_spelling": X,
+#   ""
+# }
+
+"""
+Form:
+{
+    "sequel": {
+        "noun": [
+            {
+                "definitions": "a sequel, the film after another film",
+                "anglish_word": "aftercoming",
+                "anglish_spelling": "aftercumming",
+                "forebear": "~",
+                "taken_from": "NE",
+                "notes": "",
+            }
+        ]
+    }
+}
+"""
+
+# print(all_definitions)
+        # all_definitions = {'N': ['time long past'], 'AV': ['in the past']}
+
+        # print(definitions)
+
+#         print(f"===\nWord: {word}\n\
+# Ang Spelling: {anglish_spelling}\n\
+# Meaning: {meaning}\n\
+# Kind: {kind}\n\
+# Forebear: {forebear}\n\
+# Taken from: {taken_from}\n\
+# Notes: {notes}\n\
+# \n\
+# Definitions: {definitions}")
+
+        # print(all_definitions)
--- parse_dict.py
+++ parse_dict.py
@@ -1,19 +1,6 @@
 # ᛫ delicate and tendril-like ᛫ diaphanous ᛫ gauzy ᛫ airy ᛫
 # Python 3.9 or later
 
-"""
-Word: dere
-Ang Spelling: 
-Meaning: ᛫ hurt ᛫ harm ᛫ injury ᛬ to hurt ᛫ to harm ᛫ to injure ᛫
-Kind: N᛬V
-Forebear: dēre
-Taken from: ME
-Notes: 
-
-
-Definitions: {'N': ['hurt', 'harm', 'injury'], 'V': ['to hurt', 'to harm', 'to injure']}
-"""
-
 import csv
 import json
 
@@ -33,58 +20,31 @@ def split_definitions(definitions: str, pos: str):
         # print(pos_definitions)
 
         for i, p in enumerate(poses):
-            # original_definitions = pos_definitions[i]
+            original_definitions = pos_definitions[i]
+            original_definitions = original_definitions.removeprefix("᛫ ")
+            original_definitions = original_definitions.replace(" ᛫", ",")
+            original_definitions = original_definitions.strip()
+
             # N, V, AJ
             pos_definitions[i] = pos_definitions[i].split("᛫")
             pos_definitions[i] = [d.strip() for d in pos_definitions[i]]
 
-            pos_dict[p] = pos_definitions[i]
+            pos_dict[p] = (pos_definitions[i], original_definitions)
         
         return pos_dict
     else:
+        original_definitions = definitions
+        original_definitions = original_definitions.removeprefix("᛫ ")
+        original_definitions = original_definitions.replace(" ᛫", ",")
+        original_definitions = original_definitions.strip()
+
         # There is only one parts of speech
         definitions = definitions.split("᛫")
         definitions = [d.strip() for d in definitions]
         # if definitions[0] == "": definitions.pop(0)
 
-        return {pos: definitions}
+        return {pos: (definitions, original_definitions)}
 
-# split_definitions_examples = [
-#     ["᛫ delicate and tendril-like ᛫ diaphanous ᛫ gauzy ᛫ airy ᛫", "AJ"],
-#     [" 	᛫ a region in Poland ᛫", "N(P)"],
-#     [" 	᛫ guidance ᛫ instruction ᛫", "N"],
-#     ["᛫ weak ᛫ watery ᛫ feeble wavering ᛫", "AJ"],
-#     ["᛫ a manner ᛫ a fashion ᛫ a method ᛬ to make apparent ᛬ sagacious ᛫ prudent ᛫", "N᛬V᛬AJ"]
-# ]
-
-# for s in split_definitions_examples:
-#     a = split_definitions(s[0], s[1])
-#     print(a)
-
-# Form:
-# "english definition lemma": {
-#   "anglish_word": X,
-#   "anglish_spelling": X,
-#   ""
-# }
-
-"""
-Form:
-{
-    "sequel": {
-        "noun": [
-            {
-                "definitions": "a sequel, the film after another film",
-                "anglish_word": "aftercoming",
-                "anglish_spelling": "aftercumming",
-                "forebear": "~",
-                "taken_from": "NE",
-                "notes": "",
-            }
-        ]
-    }
-}
-"""
 english_to_anglish = {}
 
 with open("the_anglish_wordbook.csv", "r") as f:
@@ -104,53 +64,47 @@ with open("the_anglish_wordbook.csv", "r") as f:
 
         all_definitions = split_definitions(meaning, kind)
 
-        # print(all_definitions)
-        # all_definitions = {'N': ['time long past'], 'AV': ['in the past']}
-
-        # print(definitions)
-
-#         print(f"===\nWord: {word}\n\
-# Ang Spelling: {anglish_spelling}\n\
-# Meaning: {meaning}\n\
-# Kind: {kind}\n\
-# Forebear: {forebear}\n\
-# Taken from: {taken_from}\n\
-# Notes: {notes}\n\
-# \n\
-# Definitions: {definitions}")
-
-        # print(all_definitions)
-
+        # Definitions:
+        # (['per'], 'per')
         for kind, definitions in all_definitions.items():
             
-            kind.replace("AC", "Acronym")
-            kind.replace("AD", "Adverb")
-            kind.replace("ADJ", "Adjective")
-            kind.replace("ADV", "Adverb")
-            kind.replace("AJ", "Adjective")
-            kind.replace("AJ(P)", "Proper Adjective")
-            kind.replace("AV", "Adverb")
-            kind.replace("C", "Conjunction")
-            kind.replace("D", "Determiner")
-            kind.replace("I", "Interjection")
-            kind.replace("N", "Noun")
-            kind.replace("N(P)", "Proper Noun")
-            kind.replace("N(PRO)", "Pronoun")
-            kind.replace("N(PN)", "Pronoun")
-            kind.replace("P", "Preposition")
-            kind.replace("PN", "Proper Noun")
-            kind.replace("PP", "Prepositional Phrase")
-            kind.replace("PHRASE", "Phrase")
-            kind.replace("PREFIX", "Prefix")
-            kind.replace("SUFFIX", "Suffix")
-            kind.replace("V", "Verb")
+            kind = kind.replace("PHRASE", "Phrase")
+            kind = kind.replace("PREFIX", "Prefix")
+            kind = kind.replace("SUFFIX", "Suffix")
+            kind = kind.replace("AJ(P)", "Proper Adjective")
+            kind = kind.replace("N(PRO)", "Pronoun")
+            kind = kind.replace("N(PN)", "Pronoun")
+            kind = kind.replace("N(P)", "Proper Noun")
+            kind = kind.replace("PP", "Prepositional Phrase")
+            kind = kind.replace("PN", "Proper Noun")
+            kind = kind.replace("AC", "Acronym")
+            kind = kind.replace("AD", "Adverb")
+            kind = kind.replace("ADJ", "Adjective")
+            kind = kind.replace("ADV", "Adverb")
+            kind = kind.replace("AJ", "Adjective")
+            kind = kind.replace("AV", "Adverb")
+            kind = kind.replace("C", "Conjunction")
+            kind = kind.replace("D", "Determiner")
+            kind = kind.replace("I", "Interjection")
+            kind = kind.replace("N", "Noun")
+            kind = kind.replace("P", "Preposition")
+            kind = kind.replace("V", "Verb")
             
-            for definition in definitions:
+            for definition in definitions[0]:
+                # "to help" → "help"
+                # "a soldier" → "soldier"
+                if kind == "Verb":
+                    definition = definition.removeprefix("to ")
+                elif kind == "Noun":
+                    definition = definition.removeprefix("a ")
+                    definition = definition.removeprefix("an ")
+
                 # If there is already an English word in the set
                 if definition in english_to_anglish:
                     # If there is already a POS of that definition
                     if kind in english_to_anglish[definition]:
                         english_to_anglish[definition][kind].append({
+                            "definitions": definitions[1],
                             "anglish_word": word,
                             "anglish_spelling": anglish_spelling,
                             "forebear": forebear,
@@ -161,6 +115,7 @@ with open("the_anglish_wordbook.csv", "r") as f:
                     else:
                         english_to_anglish[definition].update({kind: [
                             {
+                                "definitions": definitions[1],
                                 "anglish_word": word,
                                 "anglish_spelling": anglish_spelling,
                                 "forebear": forebear,
@@ -173,6 +128,7 @@ with open("the_anglish_wordbook.csv", "r") as f:
                     english_to_anglish.update({definition: {
                         kind: [
                             {
+                                "definitions": definitions[1],
                                 "anglish_word": word,
                                 "anglish_spelling": anglish_spelling,
                                 "forebear": forebear,
